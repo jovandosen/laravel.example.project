304d000233603f8d2508e00f391c33fb64b48447
jovandosen <jovandosen1994@gmail.com>
2019-11-19 13:11:06 +0100

    test controller added

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Development\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+    public function test()
+    {
+    	return view('test');
+    }
+}
---
+++ resources/views/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Test</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="{!! asset('css/test.css') !!}">
+	</head>
+	<body>
+		
+		<h1>Test</h1>
+
+		<script type="text/javascript" src="{!! asset('js/test.js') !!}"></script>
+	</body>
+</html>
